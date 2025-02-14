e0beb352be8d7c080e86f747c2ec8c85c257035f
Asrorbek <asrorbekaliulov08@gmail.com>
2025-02-15 00:00:46 +0500

    a

--- bot/BotCommands/DownDB.py
+++ bot/BotCommands/DownDB.py
@@ -7,4 +7,6 @@ from bot.decorators import admin_required
 async def DownlBD(update: Update, context: ContextTypes.DEFAULT_TYPE):
     file = open("db.sqlite3", "rb")
     await context.bot.send_document(update.effective_user.id, document=file)
+    if update.callback_query:
+        update.callback_query.answer("Yuklandi!")
     return ConversationHandler.END
